6d72c2153d027764b3be271e3b9ce335e2261e9c
Misael-Casjor <mcastro2@byupathway.edu>
2025-01-13 07:19:54 -0500

    first part of exersise one

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("What is your forst name");
+        string name = Console.ReadLine();
+        Console.WriteLine("What is your last name?");
+        Console.WriteLine(name);
     }
 }
