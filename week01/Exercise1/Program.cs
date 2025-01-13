d5544dcfb0aca498734a1a23e74a7d516112ff5e
Misael-Casjor <mcastro2@byupathway.edu>
2025-01-13 07:29:06 -0500

    fixed typing errors

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What is your forst name");
+        Console.WriteLine("What is your first name?");
         string name = Console.ReadLine();
         Console.WriteLine("What is your last name?");
         string lastName= Console.ReadLine();
