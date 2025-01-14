dd81506fb5e52303e7e927eb22899f04fe7b4dda
Misael-Casjor <mcastro2@byupathway.edu>
2025-01-13 20:43:17 -0500

    added while and if statements

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,9);
+        Console.WriteLine("Choose a number between 1 to 9");
+        string selectedNumber = Console.ReadLine();
+        int number = int.Parse(selectedNumber);
+         
+        while (number != magicNumber)
+        {
+            if (number < magicNumber)
+            {
+                Console.WriteLine("Higer");
+            }
+             if (number > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+        }
+
     }
 }
