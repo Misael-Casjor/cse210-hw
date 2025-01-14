9692b529bb989a03d22fe75475f199565cbc97d1
Misael-Casjor <mcastro2@byupathway.edu>
2025-01-13 19:45:08 -0500

    congrats and encourage

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
-        Console.WriteLine("Enter the your percentage grade");
+        Console.WriteLine("Enter the your percentage grade:");
         string percentage = Console.ReadLine();
         int forCalculation = int.Parse(percentage);
         string message = "Your grade is";
@@ -13,16 +13,33 @@ class Program
         {
             Console.Write($"{message} F");
         }
-        if (forCalculation >= 60)
+        else if (forCalculation >= 60)
         {
             Console.Write($"{message} D");
         }
-        if (forCalculation >= 70)
+        else if (forCalculation >= 70)
         {
             Console.Write($"{message} C");
 
         }
-        
+        else if (forCalculation >= 80)
+        {
+            Console.Write($"{message} B");
+
+        }
+        else if (forCalculation >= 90)
+        {
+            Console.Write($"{message} A");
+
+        }
+        if (forCalculation >= 70)
+        {
+            Console.Write(". Congratulation! you passed");
+        }
+        else if (forCalculation < 70)
+        {
+            Console.Write(", dint pass this time, remember to keep going you can do it");
+        }
 
     }
 }
