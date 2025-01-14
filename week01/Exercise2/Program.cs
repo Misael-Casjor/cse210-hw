41e5416b0ea22c6770bb4c17f70ff6afeb8e42f6
Misael-Casjor <mcastro2@byupathway.edu>
2025-01-13 19:37:24 -0500

    half activity 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,24 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Enter the your percentage grade");
+        string percentage = Console.ReadLine();
+        int forCalculation = int.Parse(percentage);
+        string message = "Your grade is";
+        if (forCalculation < 60)
+        {
+            Console.Write($"{message} F");
+        }
+        if (forCalculation >= 60)
+        {
+            Console.Write($"{message} D");
+        }
+        if (forCalculation >= 70)
+        {
+            Console.Write($"{message} C");
+
+        }
+        
+
     }
 }
