d5cc6e3cf3423d866895313acb4960dbe86fc1de
Misael-Casjor <mcastro2@byupathway.edu>
2025-01-14 06:37:28 -0500

    added list and input

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Enter a lis of numbers, type 0 to stop");
+        string number = Console.ReadLine();
+        int intnumber = int.Parse(number);
+
+        List<int> numbers = new List<int>();
+        
+        if (intnumber != 0)
+        {
+            
+        }
     }
 }
