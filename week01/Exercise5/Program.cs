6e285eb06f8b8125e0a7b672225678165eba0d33
Misael-Casjor <mcastro2@byupathway.edu>
2025-01-15 06:08:30 -0500

    added two functions

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,8 +2,18 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        void  DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+        static string PromtUserName()
+        {
+            Console.WriteLine("Enter user name:");
+            string userName = Console.ReadLine();
+            return  userName;
+        }
     }
 }
