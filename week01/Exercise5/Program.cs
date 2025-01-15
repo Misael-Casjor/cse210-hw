dee3d8e7a2d5306695605731010018be2b358960
Misael-Casjor <mcastro2@byupathway.edu>
2025-01-15 06:26:56 -0500

    Exercise 5 completed

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 
 class Program
 {
@@ -15,5 +16,26 @@ class Program
             string userName = Console.ReadLine();
             return  userName;
         }
+        static int PromptUserNumber()
+        {
+            Console.WriteLine("enter you favorite number");
+            string userNumber = Console.ReadLine();
+            int number =  int.Parse(userNumber);
+            return number;
+        }
+        static int SquareNumber(int number)
+        {
+            Console.WriteLine("Enter a number to square");
+            string numberGiven = Console.ReadLine();
+            int numberint = int.Parse(numberGiven);
+            int numberSquared = numberint * numberint;
+            return numberSquared;
+        }
+        static void DisplayResult(string name, int numberSquared)
+        {
+
+            Console.WriteLine($"your name is {name} and squared number is {numberSquared}");
+        }
+
     }
 }
