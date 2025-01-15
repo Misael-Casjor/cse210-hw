7ca1a361f26f09f43d484678654dcde0210a87ec
Misael-Casjor <mcastro2@byupathway.edu>
2025-01-15 04:42:54 -0500

    starting over

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -6,25 +6,20 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
-        Console.WriteLine("Enter a list of numbers one by one, type 0 to stop");
+        
 
 
         List<int> numbers = new List<int>();
         int number = -1;
-        do
-        {        
-         Console.Write("Enter a Number for your list");
-         string numberstr = Console.ReadLine();
-         number = int.Parse(numberstr);
-         if (number != 0)
-         {
-            numbers.Add(number);
-         }
 
+        while (number != 0)
+        {
+            Console.WriteLine("enter a number, then enter a 0 to stop");
+            string strNumber = Console.ReadLine();
+            number = int.Parse(strNumber);
 
-        } while (number != 0);    
-          Console.Write("Here is your list:");
-          Console.WriteLine(numbers);
+        }
+
 
         
     }     
