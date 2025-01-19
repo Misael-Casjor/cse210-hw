4b196502b7fa119c3946f665e1f878e83bb8a176
Misael-Casjor <mcastro2@byupathway.edu>
2025-01-19 18:53:55 -0500

    added new display

---
+++ week02/Resumes/job.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Threading.Tasks.Dataflow;
+
+  public class Job()
+ {
+   public string _company;
+   public string _jobTitle;
+   public string _startYear; 
+   public string _endYear; 
+
+   public void DisplayJobDetails()
+   {
+    Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear} ");
+   }
+ }
