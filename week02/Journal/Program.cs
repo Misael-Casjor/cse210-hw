fc7ce5c4e2e6a7ab7fa1ddb9e452539be8ba87c8
Misael-Casjor <mcastro2@byupathway.edu>
2025-01-20 04:57:11 -0500

    added a menu

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Journal Project.");
+
+        Console.WriteLine("1.-New entry");
+        Console.WriteLine("2.-Display the journal");
+        Console.WriteLine("3.-Save the journal to a file");
+        Console.WriteLine("4.-Load the journal to a file");
+        Console.WriteLine("5.-CLose the program");
+        Console.WriteLine("Choose your option");
+        string chosen = Console.ReadLine();
+
+        while (chosen != "5")
+        {
+        Console.WriteLine("1.-New entry");
+        Console.WriteLine("2.-Display the journal");
+        Console.WriteLine("3.-Save the journal to a file");
+        Console.WriteLine("4.-Load the journal to a file");
+        Console.WriteLine("5.-CLose the program");
+        Console.WriteLine("Choose your option");
+        chosen = Console.ReadLine();
+
+        
+
+        }
+
     }
 }
