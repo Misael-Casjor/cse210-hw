134597febe888aac6c76f5657be4c055acfe1b74
Misael-Casjor <mcastro2@byupathway.edu>
2025-01-20 21:07:35 -0500

    trying to implement a random display for prompts

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -17,16 +17,25 @@ class Program
 
         while (chosen != "5")
         {
-        Console.WriteLine("1.-New entry");
-        Console.WriteLine("2.-Display the journal");
-        Console.WriteLine("3.-Save the journal to a file");
-        Console.WriteLine("4.-Load the journal to a file");
-        Console.WriteLine("5.-CLose the program");
-        Console.WriteLine("Choose your option");
-        chosen = Console.ReadLine();
+            Console.WriteLine("1.-New entry");
+            Console.WriteLine("2.-Display the journal");
+            Console.WriteLine("3.-Save the journal to a file");
+            Console.WriteLine("4.-Load the journal to a file");
+            Console.WriteLine("5.-CLose the program");
+            Console.WriteLine("Choose your option");
+            chosen = Console.ReadLine();
 
-        
+            if (chosen == "1")
+            {
+                Prompts promptaccess = new Prompts(); 
+                List<string> items = promptaccess._prompt;
+                Random pick = new Random();
+                int randompick = pick.Next(_prompt.5);
+                string randomprompt = _prompt[randompick];
+                Console.WriteLine(randomprompt);
 
+            
+            }
         }
 
     }
