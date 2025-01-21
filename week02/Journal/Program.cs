9c19e9d44c130568233e353a84ffa4dead7ea81c
Misael-Casjor <mcastro2@byupathway.edu>
2025-01-20 21:29:02 -0500

    finished the prompt display

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,19 +1,13 @@
 using System;
+using System.Formats.Asn1;
 using System.Runtime.InteropServices;
+using System.Threading.Channels;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
-
-        Console.WriteLine("1.-New entry");
-        Console.WriteLine("2.-Display the journal");
-        Console.WriteLine("3.-Save the journal to a file");
-        Console.WriteLine("4.-Load the journal to a file");
-        Console.WriteLine("5.-CLose the program");
-        Console.WriteLine("Choose your option");
-        string chosen = Console.ReadLine();
+        string chosen = "0";
 
         while (chosen != "5")
         {
@@ -29,10 +23,12 @@ class Program
             {
                 Prompts promptaccess = new Prompts(); 
                 List<string> items = promptaccess._prompt;
+
                 Random pick = new Random();
-                int randompick = pick.Next(_prompt.5);
-                string randomprompt = _prompt[randompick];
+                int randompick = pick.Next(items.Count);
+                string randomprompt = items[randompick];
                 Console.WriteLine(randomprompt);
+                string answer = Console.ReadLine();
 
             
             }
