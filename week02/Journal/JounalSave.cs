bcf7cad6d33b3b0aeac777107194537ea0edd211
Misael-Casjor <mcastro2@byupathway.edu>
2025-01-22 05:27:05 -0500

    added an date saver

--- week02/Journal/JounalSave.cs
+++ week02/Journal/JounalSave.cs
@@ -1,14 +1,22 @@
 using System.IO;
 
+
 public static class FilesSaver
 {
+
+    public static string DateSaved()
+        {
+        DateTime theCurrentTime = DateTime.Now;
+        return theCurrentTime.ToShortDateString();
+        } 
     public static void SaveToFile(string answer)
         {
         string filename = "Save.txt";
+        string dateText = DateSaved();
 
         using (StreamWriter outputFile = new StreamWriter(filename))
             {
-                outputFile.WriteLine(answer);
+                outputFile.WriteLine($"Date: {dateText}Entry:{answer}");
             }
         }  
 }
