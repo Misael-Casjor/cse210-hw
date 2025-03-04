1dc1e7856d9906ccb7064a0d90516b0d4fcbe957
Misael-Casjor <mcastro2@byupathway.edu>
2025-03-04 05:43:01 -0500

    exersise one week one finished

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        string firstName;
+        string lastName;
+        Console.WriteLine("What is your first name?");
+        firstName = Console.ReadLine();
+        Console.WriteLine("What is your last name?");
+        lastName = Console.ReadLine();
+        Console.WriteLine($"Your Name is {lastName}, {firstName} {lastName}");
+
+
     }
 }
