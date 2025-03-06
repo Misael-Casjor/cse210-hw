51fee6071073fa97d6486cbd6b8acb2716e899ed
Misael-Casjor <mcastro2@byupathway.edu>
2025-03-06 05:10:34 -0500

    exercise 2 and exercise 3 completed

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,34 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        int userPersentage = 0;
+        string grade = "";
+        Console.WriteLine("Enter your percentage");
+        userPersentage = int.Parse(Console.ReadLine());
+
+        if (userPersentage >= 90){
+            grade = "A";
+        }
+        else if (userPersentage >= 80)
+        {
+            grade = "B";
+        }
+        else if (userPersentage >= 70)
+        {
+            grade = "C";
+        }
+        else if (userPersentage >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your grade is {grade}");
+
+
     }
 }
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,24 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        int answer = 0;
+        int tries = 0;
+        do
+        {
+             Console.WriteLine("Gess the number");
+             answer = int.Parse(Console.ReadLine());
+             tries += 1;
+             if (answer > number){
+                Console.WriteLine("Smaller");
+             }
+             else{
+                Console.WriteLine("Bigger");
+             }
+
+
+        } while (answer != number);
+        Console.WriteLine($"you gessed the number, it was {number}. Number of tries: {tries}!");
     }
 }
