684b78987ef80886a0a9cbad304a97ffb8f33b53
Misael-Casjor <mcastro2@byupathway.edu>
2025-03-11 05:13:38 -0400

    part of the activity completed

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Journal Project.");
+        Job job1 = new Job();
+        Job job2 = new Job();
+        job1._company = "Microsoft";
+        job1._company = "Apple";
+
+        
     }
 }
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        Job job2 = new Job();
+        job1._company = "Microsoft";
+        job2._company = "Apple";
+
+        Console.WriteLine(job1._company);
+        Console.WriteLine(job2._company);
     }
 }
