607cdd33d9a1c5bbe8a789f0b0d5331dff51857d
Misael-Casjor <mcastro2@byupathway.edu>
2025-03-25 05:19:26 -0400

    better entry handling and entries txt file added

--- week02/Journal/Entry.cs
+++ week02/Journal/Entry.cs
@@ -4,13 +4,14 @@ using System.Reflection.Metadata;
 public class Entry{
 
     public string _userEntry;
+  //public string _prompt; 
     public string _prompt;
-
-    public string _date = DateTime.Now.ToString("M/d/yyyy");
+  
+  public string _date = DateTime.Now.ToString("M/d/yyyy");
 
     public string EntireEntry(){
          
-      return   
+      return $"{_date}| {_prompt}: {_userEntry}";  
 
     }
 
--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,6 +1,10 @@
 using System;
 using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
+
+// extra, this program will count how many entries you have writted without beeing saved and it will
+// send a warning example: you writed 3 entries, please SAVE them before closing the program
 
 class Program
 {
@@ -8,33 +12,53 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Journal Project.");
         int option;
-
+        List<Entry> _entries = new List<Entry>();
+        int count = 0;
         do
         {
-            int count = 0;
+            
             Console.WriteLine("1. Write");
             Console.WriteLine("2. Display");
             Console.WriteLine("3. Load");
             Console.WriteLine("4. Save");
             Console.WriteLine("5. Quit");
             option = int.Parse(Console.ReadLine());
+            if (count > 0){
+                if(count == 1){
+                    Console.WriteLine($"ATTENTION you have {count} entry widout saving, remember to save your entry");
+                }
+                if(count > 1){
+                    Console.WriteLine($"ATTENTION you have {count} entries widout saving, remember to save your entries");
+                }
+            
+            }
 
 
             if (option == 1)
             {
-                PromptGenerator singlePromt = new PromptGenerator();
-                string sPrompt = singlePromt.GetRandomPrompt();
                 Entry entry = new Entry();
+                PromptGenerator singlePromt = new PromptGenerator();
+                string sPrompt = singlePromt.GetRandomPrompt(); 
+                Console.WriteLine($"{entry._date}|{sPrompt}");
+                entry._prompt = sPrompt;
                 entry._userEntry = Console.ReadLine();
-                entry._prompt =  sPrompt;
-                
+                _entries.Add(entry);
+
 
                 count +=1;
 
              }
-            if (option == 2) { }
+            if (option == 2) {
+                foreach (Entry item in _entries){
+                    Console.WriteLine(item.EntireEntry());
+                }
+                
+             }
             if (option == 3) { }
-            if (option == 4) { }
+            if (option == 4) {
+                Console.WriteLine("");
+                count = 0;
+             }
 
         } while (option != 5);
     }
