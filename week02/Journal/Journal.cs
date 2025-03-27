0e1e0bd3fb03594661f63d69e3711ee4ca937f69
Misael-Casjor <mcastro2@byupathway.edu>
2025-03-27 04:27:42 -0400

    journal file added

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -7,4 +7,62 @@ public class Journal{
     int count = 0;
     string fileName = "entries.txt";
 
+    public void EnterEntry(){
+        Entry entry = new Entry();
+        PromptGenerator singlePromt = new PromptGenerator();
+        string sPrompt = singlePromt.GetRandomPrompt();
+        Console.WriteLine($"{entry._date}|{sPrompt}");
+        entry._prompt = sPrompt;
+        entry._userEntry = Console.ReadLine();
+        _entries.Add(entry);
+
+
+        count += 1;
+    }
+   
+    public void DisplayEntries(){
+        foreach (Entry item in _entries)
+        {
+            Console.WriteLine(item.EntireEntry());
+        }
+        foreach (string loaded in loadedFile)
+        {
+            Console.WriteLine(loaded);
+        }
+
+    }
+
+    public void LoadEntries(){
+        loadedFile.Clear();//I added this so the list gets overwrited instead of duplicated when you try to 
+                           //load the file twice 
+        string[] lines = System.IO.File.ReadAllLines(fileName);// from the assigment page
+
+        foreach (string line in lines)
+        {
+            loadedFile.Add(line);
+        }
+        Console.WriteLine($"the file ({fileName}) has been loaded succesfully");
+    }
+    public void SaveEntries(){
+
+        using (StreamWriter outputFile = new StreamWriter(fileName, true))
+            foreach (Entry item in _entries)
+            {
+                outputFile.WriteLine(item.EntireEntry());
+            }
+        Console.WriteLine("the entries have been saved succesfully!");
+        _entries.Clear();
+        count = 0;
+
+    }
+    public void ShowSaveWarning(){
+        if (count == 1)
+        {
+            Console.WriteLine($"ATTENTION you have {count} entry widout saving, remember to save your entry");
+        }
+        if (count > 1)
+        {
+            Console.WriteLine($"ATTENTION you have {count} entries widout saving, remember to save your entries");
+        }
+    }
 }
--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -3,7 +3,7 @@ using System.Reflection.Metadata;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
 
-// extra, this program will count how many entries you have writted without beeing saved and it will
+// Extra, this program will count how many entries you have writted without beeing saved and it will
 // send a warning example: you writed 3 entries, please SAVE them before closing the program
 
 class Program
@@ -12,10 +12,10 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Journal Project.");
         int option;
-        List<Entry> _entries = new List<Entry>();
-        List<string> loadedFile = new List<string>();
-        int count = 0;
-        string fileName = "entries.txt";
+ 
+
+        Journal journal = new Journal();
+
         do
         {
             
@@ -25,78 +25,32 @@ class Program
             Console.WriteLine("4. Save");
             Console.WriteLine("5. Quit");
             option = int.Parse(Console.ReadLine());
-            if (count > 0){
-                if(count == 1){
-                    Console.WriteLine($"ATTENTION you have {count} entry without saving, remember to save your entry");
-                }
-                if(count > 1){
-                    Console.WriteLine($"ATTENTION you have {count} entries without saving, remember to save your entries");
-                }
-            
-            }
 
+            
+            
 
             if (option == 1)
             {
-                Entry entry = new Entry();
-                PromptGenerator singlePromt = new PromptGenerator();
-                string sPrompt = singlePromt.GetRandomPrompt(); 
-                Console.WriteLine($"{entry._date}|{sPrompt}");
-                entry._prompt = sPrompt;
-                entry._userEntry = Console.ReadLine();
-                _entries.Add(entry);
-
-
-                count +=1;
+             journal.EnterEntry();
             }
 
-            if (option == 2) {
-                foreach (Entry item in _entries){
-                    Console.WriteLine(item.EntireEntry());
-                }
-                foreach (string loaded in loadedFile){
-                    Console.WriteLine(loaded);
-                }
+            else if (option == 2) {
+              journal.DisplayEntries();
              }
-            if (option == 3) {
-                loadedFile.Clear();//I added this so the list gets overwrited instead of duplicated when you try to 
-                //load the file twice 
-                string[] lines = System.IO.File.ReadAllLines(fileName);// from the assigment page
 
-                foreach (string line in lines)
-                {
-                    loadedFile.Add(line);
-                }
-                Console.WriteLine($"the file ({fileName}) has been loaded succesfully");
+            else if (option == 3) {
+               journal.LoadEntries();
 
             }
-            if (option == 4) {
-                
-                using (StreamWriter outputFile = new StreamWriter(fileName, true))
-                    foreach (Entry item in _entries)
-                    {
-                        outputFile.WriteLine(item.EntireEntry());
-                    }
-                    Console.WriteLine("the entries have been saved succesfully!");
-                    _entries.Clear();
-                count = 0;
+            else if (option == 4) {
+                journal.SaveEntries();
+
              }
 
 
-            if (count > 0)
-            {
-                if (count == 1)
-                {
-                    Console.WriteLine($"ATTENTION you have {count} entry widout saving, remember to save your entry");
-                }
-                if (count > 1)
-                {
-                    Console.WriteLine($"ATTENTION you have {count} entries widout saving, remember to save your entries");
-                }
 
-            }
 
-            if (option > 5){
+            else {
                 Console.WriteLine("please enter a number between 1 and 5");
             }
 
