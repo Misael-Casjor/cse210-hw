341c4832f437337e4cc46316772ca1eba836ff4c
Misael-Casjor <mcastro2@byupathway.edu>
2025-07-03 16:56:03 -0400

    if statements exersice 2 added

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
+
+        int pGrade;
+        string grade;
+
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.WriteLine("What is your grade percentage?");
+        pGrade = int.Parse(Console.ReadLine());
+
+        if (pGrade >= 90)
+        {
+            grade = "A";
+        }
+
+        if (pGrade >= 80)
+        {
+            grade = "B";
+        }
+
+        if (pGrade >= 70)
+        {
+            grade = "C";
+        }
+
+        if (pGrade >= 60)
+        {
+            grade = "D";
+        }
+
+        if (pGrade > 50)
+        {
+            grade = "F";
+        }
+        
+        
     }
 }
