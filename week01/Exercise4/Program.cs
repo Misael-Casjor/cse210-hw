d77088b2bc3154c19061602bdcf835b6ecad0b85
Misael-Casjor <mcastro2@byupathway.edu>
2025-07-05 06:50:08 -0400

    base for exercice 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.ComponentModel.Design;
+using System.Transactions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+        List<int> numbers;
+        numbers = new List<int>();
+        int toAdd;
+        Console.WriteLine("Enter a list of numbers, type 0 when youre done");
+        do
+        {
+            Console.WriteLine("Enter the number you want to add:");
+            toAdd = int.Parse(Console.ReadLine());
+            if (toAdd != 0)
+            {
+                numbers.Add(toAdd);
+            }
+        } while (toAdd != 0);
+
+        Console.WriteLine("this is your list");
+        foreach (int item in numbers)
+        {
+            Console.WriteLine(item);
+        }
+
     }
 }
