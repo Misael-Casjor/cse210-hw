1efe945bfa8e813e9f9eb78842ddde3293f6d02c
Misael-Casjor <mcastro2@byupathway.edu>
2025-07-05 05:16:41 -0400

    exercice 3 without the automatic pick

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Transactions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        //gess my number game
+        int magicNumber;
+        int response;
+        Console.WriteLine("enter the magic number");
+        magicNumber = int.Parse(Console.ReadLine());
+        do
+        {
+            Console.WriteLine("Enter your gess");
+            response = int.Parse(Console.ReadLine());
+            if (response > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (response < magicNumber)
+            {
+                Console.WriteLine("Higer");
+            }
+        } while (response != magicNumber);
+        Console.WriteLine("You gessed it");
     }
 }
