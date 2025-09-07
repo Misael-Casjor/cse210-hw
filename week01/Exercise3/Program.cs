eeadb977be73c2a88ac9e6323ba1052e35f418d6
Misael-Casjor <mcastro2@byupathway.edu>
2025-09-07 00:36:58 -0400

    Week 1 exercise 3 completed

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,43 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random magicNumber = new Random();
+     
+        int gess = 0;
+        
+        string playAgain = "yes";
+        Console.WriteLine("Gess a number from 1 to 100");
+        do
+        {
+            int number = magicNumber.Next(1, 101);
+            int count = 0;
+
+            while (number != gess)
+            {
+                Console.WriteLine("make a gess");
+                gess = int.Parse(Console.ReadLine());
+                count += 1;
+
+                if (gess > number)
+                {
+                    Console.WriteLine("Smaller");
+                }
+                else if (gess < number)
+                {
+                    Console.WriteLine("Bigger");
+                }
+                if (number == gess)
+                {
+                    Console.WriteLine($"You guessed the number! it was {number}, you gessed {count} times");
+                    Console.WriteLine("Wanna play again?(type 'yes' to continue)");
+                    playAgain = Console.ReadLine();
+                }
+            }
+            
+        } while (playAgain == "yes");
+
+        Console.WriteLine("See you later!");
+
+
     }
 }
