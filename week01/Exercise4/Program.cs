213387bd156c444eb3d06b7959437fb31baae839
Misael-Casjor <mcastro2@byupathway.edu>
2025-09-08 01:38:35 -0400

    week 1 exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,66 @@
 using System;
+using System.Dynamic;
+using System.Globalization;
+using System.Runtime.InteropServices.Marshalling;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+        List<int> numberList = new List<int>();
+        int number = 0;
+        do
+        {
+            Console.WriteLine("Enter a list of numbers, type 0 when finished");
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
+            {
+                numberList.Add(number);
+                Console.WriteLine($"{number} had been added to the list succesfully!");
+            }
+
+
+        } while (number != 0);
+        Console.WriteLine("exiting the loop mode");
+
+        int smaller = 999999999;
+        int bigger = -99999999;
+        int sum = 0;
+        for (int i = 0; i < numberList.Count; i++)
+        {
+
+            int fromList = numberList[i];
+            Console.WriteLine(fromList);
+            if (smaller > fromList & fromList > 0)
+            {
+                smaller = fromList;
+            }
+            if (bigger < fromList)
+            {
+                bigger = fromList;
+            }
+            sum += fromList;
+
+        }
+
+
+        double average = (double)sum / numberList.Count;
+        Console.WriteLine($"the smallest positive number is {smaller}");
+        Console.WriteLine($"the bigest number is {bigger}");
+        Console.WriteLine($"the sum of the numbers is {sum}");
+        Console.WriteLine($"this is the average {average}");
+        Console.WriteLine($"here is the sorted List");
+        numberList.Reverse();
+        for (int i = 0; i < numberList.Count; i++)
+        {
+
+            int fromList = numberList[i];
+            Console.WriteLine(fromList);
+
+
+        }
     }
 }
