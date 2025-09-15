5bd6ab537944b5595f0d8a7a98d640802cd3c5fd
Misael-Casjor <mcastro2@byupathway.edu>
2025-09-15 02:40:43 -0400

    added Promptgenerator file

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -5,5 +5,8 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Journal Project.");
+        PromptGenerator prompt = new PromptGenerator();
+        string _thePrompt = prompt.RandomPrompt();
+        Console.WriteLine(_thePrompt);
     }
 }
---
+++ week02/Journal/PromptGenerator.cs
@@ -0,0 +1,29 @@
+public class PromptGenerator
+{
+    public List<string> _prompts = new List<string>();
+    Random _randomP = new Random();
+
+   
+    public PromptGenerator()
+    {
+
+        _prompts.Add("Something that are you gratefull about");
+        _prompts.Add("If you can change something from today, what it culd be?");
+        _prompts.Add("Any new goal that do you want to achieve");
+        _prompts.Add("What would you like to do better tomorrow?");
+        _prompts.Add("What maked you smile today?");
+        
+
+    }
+
+
+
+    public string RandomPrompt()
+    {
+        int _index = _randomP.Next(_prompts.Count);
+        string _thePrompt = _prompts[_index];
+        return _thePrompt;
+    }
+
+
+}
