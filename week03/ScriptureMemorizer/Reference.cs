cb8b0977e5be0f8228f61ac6cf585a62579f5c3a
Misael-Casjor <mcastro2@byupathway.edu>
2025-09-23 16:12:47 -0400

    reference and Word classes completed

--- week03/ScriptureMemorizer/Reference.cs
+++ week03/ScriptureMemorizer/Reference.cs
@@ -5,4 +5,26 @@ public class Reference
     int _verse;
     int _endverse;
 
+
+    public Reference(string book, int chapter, int verse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+
+    }
+    public Reference(string book, int chapter, int startverse, int endverse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = startverse;
+        _endverse = endverse;
+    }
+
+
+    public string getDisplayText()
+    {
+        return $"{_book} {_chapter} {_verse} {_endverse}";
+    }
+
 }
--- week03/ScriptureMemorizer/Word.cs
+++ week03/ScriptureMemorizer/Word.cs
@@ -1,7 +1,29 @@
 public class Word
 {
-    string _text;
-    bool _isHidden;
+    private string _text;
+    private bool _isHidden;
 
-    
+    public Word(string text)
+    {
+        _text = text;
+    }
+
+    public void Hide()
+    {
+        _isHidden = true;
+    }
+    public void Show()
+    {
+        _isHidden = false;
+    }
+
+    public bool isHidden()
+    {
+        return _isHidden;
+    }
+
+    public string getDisplayText()
+    {
+        return _text;
+    }
 }
